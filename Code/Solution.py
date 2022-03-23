e227886951034816b608741944b11f26205c696e
oliviaret <64926459+oliviaret@users.noreply.github.com>
2022-03-22 20:27:48 -0700

    Update Solution.py

--- Code/Solution.py
+++ Code/Solution.py
@@ -1,4 +1,3 @@
-from matplotlib import pyplot as plt
 def GetLocation(move_type, env, current_frame):
     time.sleep(1) #artificial one second processing time
     
@@ -15,7 +14,7 @@ def GetLocation(move_type, env, current_frame):
         North-West = 7
         NOOP = 8
         """
-        coordinate = env.action_space.sample() 
+        coordinate = env.action_space.sample()  
     #Use absolute coordinates for the position of the "gun", coordinate space are defined below
     else:
         """
@@ -23,42 +22,32 @@ def GetLocation(move_type, env, current_frame):
         Upper left = (0,0)
         Bottom right = (W, H) 
         """
-        #note, couldn't get the wget to work so just uploaded it manually
+        
         duck = cv2.cvtColor(cv2.imread('Level_1.png'),cv2.COLOR_RGB2GRAY)
         sift = cv2.SIFT_create();
 
-        frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        #frame = cv2.resize(cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY), None, fx =0.5, fy = 0.5, interpolation = cv2.INTER_CUBIC)
         kp1, des1 = sift.detectAndCompute(duck,None)
         kp2, des2 = sift.detectAndCompute(frame,None)
 
         bf = cv2.BFMatcher()
         matches = bf.knnMatch(des1,des2,k=2)
 
-        indexes = []
-        
-        
+        positions = []        
+        #cv2.imwrite("frame.png", frame)
         for m,n in matches:
-          if m.distance < 0.9*n.distance:
-            indexes.append(m.trainIdx)
-            
-        print("num match", len(indexes))
-
-        positions = []
-        for ip in range(len(indexes)):
-          
-          pos = np.array(kp2[ip].pt)
-          pos1 = np.array(kp2[ip].pt)
-          if(int(pos[1]) !=641 and pos[1] > 20):
-            pos1[0] = pos[1]
-            pos1[1] = pos[0]
-            positions.append(pos1)
-        
-
-        
+          if m.distance < 0.685*n.distance:
+            pos = np.array(kp2[m.trainIdx].pt)
+            pos1 = np.array(kp2[m.trainIdx].pt)  
+            pos1[0] = round(pos[1]*2)
+            pos1[1] = round(pos[0]*2)
+            positions.append(pos1)  
+        print("num match", len(positions))
+        possy = np.expand_dims(np.array(positions),axis=0)
         if(len(positions)==0):
           coordinate = [0,0]
         else:
-          coordinate = positions[0]
+          coordinate = np.mean(positions, axis = 0)
         print(coordinate)
         
     
