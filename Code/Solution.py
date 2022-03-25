4b094ff9e988966f7ecb6cd6f89c6ba64aadd449
oliviaret <64926459+oliviaret@users.noreply.github.com>
2022-03-24 20:24:09 -0700

    Update Solution.py

--- Code/Solution.py
+++ Code/Solution.py
@@ -1,5 +1,5 @@
 def GetLocation(move_type, env, current_frame):
-    time.sleep(1) #artificial one second processing time
+    #time.sleep(1) #artificial one second processing time
     
     #Use relative coordinates to the current position of the "gun", defined as an integer below
     if move_type == "relative":
@@ -23,10 +23,10 @@ def GetLocation(move_type, env, current_frame):
         Bottom right = (W, H) 
         """
         
-        duck = cv2.cvtColor(cv2.imread('Level_1.png'),cv2.COLOR_RGB2GRAY)
+        duck = cv2.cvtColor(cv2.imread('ducks.png'),cv2.COLOR_RGB2GRAY)
         sift = cv2.SIFT_create();
 
-        frame = cv2.resize(cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY), None, fx =0.5, fy = 0.5, interpolation = cv2.INTER_CUBIC)
+        frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         kp1, des1 = sift.detectAndCompute(duck,None)
         kp2, des2 = sift.detectAndCompute(frame,None)
 
@@ -34,21 +34,18 @@ def GetLocation(move_type, env, current_frame):
         matches = bf.knnMatch(des1,des2,k=2)
 
         positions = []        
-        #cv2.imwrite("frame.png", frame)
+        cv2.imwrite("frame.png", frame)
         for m,n in matches:
-          if m.distance < 0.685*n.distance:
+          if m.distance < 0.59*n.distance:
             pos = np.array(kp2[m.trainIdx].pt)
             pos1 = np.array(kp2[m.trainIdx].pt)  
-            pos1[0] = round(pos[1]*2)
-            pos1[1] = round(pos[0]*2)
+            pos1[0] = round(pos[1])
+            pos1[1] = round(pos[0])
             positions.append(pos1)  
-        print("num match", len(positions))
+        #print("num match", len(positions))
         possy = np.expand_dims(np.array(positions),axis=0)
         if(len(positions)==0):
           coordinate = [0,0]
         else:
           coordinate = np.mean(positions, axis = 0)
-        print(coordinate)
-        
-    
-    return [{'coordinate' : coordinate, 'move_type' : move_type}]
+        #print(coordinate)
